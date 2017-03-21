d4ee99da572a7bd93c07483e3098cd3c65ec93f7
Henrique de Almeida <henriquealmeida1492@gmail.com>
2017-03-21 16:43:17 -0300

    Implements java class InfoFav

--- app/src/main/java/com/camposbus/soulcode/camposbus/InfoFav.java
+++ app/src/main/java/com/camposbus/soulcode/camposbus/InfoFav.java
@@ -5,4 +5,29 @@ package com.camposbus.soulcode.camposbus;
  */
 
 public class InfoFav {
+
+    private String linhaNome;
+    private boolean isFav;
+
+    public InfoFav(String nameInput){
+        linhaNome = nameInput;
+        isFav = false;
+    }
+
+    public String getName(){
+        return linhaNome;
+    }
+
+    public boolean getFavStatus(){
+        return isFav;
+    }
+
+    public void setFavStatus(){
+        if(isFav){
+            isFav = false;
+        }
+        else{
+            isFav = true;
+        }
+    }
 }
